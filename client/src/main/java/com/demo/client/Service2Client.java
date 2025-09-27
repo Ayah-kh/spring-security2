3d7d89791022aaf97425e9a45526b7656ade5c37
Ayah Alkhdarat <ayah.alkhdarat@digitinary.com>
2025-09-27 12:24:17 +0300

    add controller and service

--- client/src/main/java/com/demo/client/Service2Client.java
+++ client/src/main/java/com/demo/client/Service2Client.java
@@ -1,13 +1,17 @@
 package com.demo.client;
 
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,16 +31,35 @@ public class Service2Client {
     }
 
     public String fetchData() {
-        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
-                .withClientRegistrationId("keycloak-client")
-                .principal("authorizeRequest")
-                .build();
+//        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+//                .withClientRegistrationId("keycloak-client")
+//                .principal("authorizeRequest")
+//                .build();
+//
+//        OAuth2AuthorizedClient client = manager.authorize(authorizeRequest);
+//        String token = client.getAccessToken().getTokenValue();
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setBearerAuth(token);
+//
+//        ResponseEntity<String> response = restTemplate.exchange(service2Url + "/data",
+//                HttpMethod.GET,
+//                new HttpEntity<>(httpHeaders),
+//                String.class);
+//
+//        return response.getBody();
 
-        OAuth2AuthorizedClient client = manager.authorize(authorizeRequest);
-        String token = client.getAccessToken().getTokenValue();
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+
+        String incomingToken = null;
+
+        if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
+            incomingToken = jwtAuthenticationToken.getToken().getTokenValue();
+        }
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBearerAuth(token);
+        httpHeaders.setBearerAuth(incomingToken);
 
         ResponseEntity<String> response = restTemplate.exchange(service2Url + "/data",
                 HttpMethod.GET,
