5e5fb6b55e2073293b69d3fb1551b3b810807718
Ayah Alkhdarat <ayah.alkhdarat@digitinary.com>
2025-09-27 12:24:43 +0300

    add pkce project example

---
+++ pkce-demo/src/main/java/com/demo/pkce/demo/PkceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pkce.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkceDemoApplication.class, args);
+	}
+
+}
---
+++ pkce-demo/src/test/java/com/demo/pkce/demo/PkceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pkce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
